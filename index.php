ae7d2704002bea370ab9441f792123315d36ef4e
WallaceWebs <25995763+WallaceWebsBR@users.noreply.github.com>
2019-02-26 17:45:25 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$url = $_GET['url'];
+
+echo $url;
+?>
+
+<html>
+  <head>
+    <title>Youtube Views BOT by WallaceWebs</title>
+  </head>
+  <body>
+  </body>
+      <form action="/" method="GET">
+      YoutTube URL: <input type="text" name="url"><br />
+      <input type="submit" placeholder="SEND IT">
+      </form>
+</body>
+</html>
