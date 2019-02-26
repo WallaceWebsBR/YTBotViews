b09d0b7343b050280e776e2a261824b03ceeba4c
WallaceWebs <25995763+WallaceWebsBR@users.noreply.github.com>
2019-02-26 18:07:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
+//TO DEBUG ERRORS
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
+
 $url = $_GET['url'];
+$file = file_get_contents('http://image.thum.io/get/'.$url);
 
-echo $url;
+echo "VISIT GENERATED";
 ?>
-
 <html>
   <head>
     <title>Youtube Views BOT by WallaceWebs</title>
   </head>
   <body>
   </body>
-      <form action="/" method="GET">
+      <form action="#" method="GET">
       YoutTube URL: <input type="text" name="url"><br />
       <input type="submit" placeholder="SEND IT">
       </form>
