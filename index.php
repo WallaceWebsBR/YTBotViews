464571c04e65a46b1a6f30bb4cb4f9e17065d32a
WallaceWebs <25995763+WallaceWebsBR@users.noreply.github.com>
2019-02-26 18:40:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,25 @@ ini_set('display_errors',1);
 ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
 
-//GET URL AND MAKE REQUEST TO GENERATE SCREENSHOT OF PAGE
+//FUNCTIONS
+function proxy($proxy, $urlget){
+$url = 'http://image.thum.io/get/'.$urlget;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+$curl_scraped_page = curl_exec($ch);
+curl_close($ch);
+}
+
+//GET URL AND PROXY TO MAKE REQUEST TO GENERATE SCREENSHOT OF PAGE
+$proxy = $_GET['proxy'];
 $url = $_GET['url'];
-$file = file_get_contents('http://image.thum.io/get/'.$url);
+
+proxy($proxy, $url);
 
 //RESULT
 echo "+1 VISIT GENERATED!!!";
@@ -19,6 +35,7 @@ echo "+1 VISIT GENERATED!!!";
   </body>
       <form action="#" method="GET">
       YoutTube URL: <input type="text" name="url"><br />
+      ProxyURL(IP:PORT) : <input tyoe="text" name="proxy"><br />
       <input type="submit" placeholder="SEND IT">
       </form>
 </body>
